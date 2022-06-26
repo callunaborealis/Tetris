626550795f298469ace32c2a6b05526f95dd41ef
callunaborealis <callunaborealis@gmail.com>
2022-06-26 15:03:23 +0800

    Add block and rest of block subclasses and blockqueue class

---
+++ TetrisWPF/BlockQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisWPF
+{
+    internal class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IShapedBlock(),
+            new JShapedBlock(),
+            new LShapedBlock(),
+            new SquareBlock(),
+            new TShapedBlock(),
+            new ZShapedBlock()
+        };
+
+        private readonly Random random = new();
+
+        public Block NextBlock { get; private set; }
+
+        private Block GetRandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public BlockQueue()
+        {
+            NextBlock = GetRandomBlock();
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+            do
+            {
+                NextBlock = GetRandomBlock();
+            }
+            while (block.Id != NextBlock.Id);
+
+            return block;
+        }
+    }
+}
